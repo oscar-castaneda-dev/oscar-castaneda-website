101bff40c0efe44f0a35bd7094eb9616b20b2d9d
Oscar Castaneda <oscar.castaneda.developer@gmail.com>
2025-05-11 23:19:17 -0600

    feat(blog): implement MDX support for blog posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,23 @@
 import { ModeToggle } from "@/components/ui/mode-toggle";
+import { getPosts } from "@/lib/postLoader";
+import Link from "next/link";
 
-export default function Home() {
+export default async function Home() {
+  const posts = await getPosts();
+
   return (
-    <div className="flex items-center gap-x-4 p-4">
-      <h1>Hello World</h1>
-      <ModeToggle />
+    <div className="p-4">
+      <div className="flex items-center gap-x-4">
+        <h1>Hello World</h1>
+        <ModeToggle />
+      </div>
+      <div>
+        {posts.map((post) => (
+          <Link key={post.slug} href={`/blog/${post.slug}`}>
+            {post.title}
+          </Link>
+        ))}
+      </div>
     </div>
   );
 }
---
+++ lib/postLoader.tsx
@@ -0,0 +1,61 @@
+import { remark } from "remark";
+import fs from "fs";
+import html from "remark-html";
+import matter from "gray-matter";
+import path from "path";
+
+import type { Post } from "@/types/posts";
+
+const postsDirectory = path.join(process.cwd(), "posts");
+
+export async function getPosts(): Promise<Post[]> {
+  const fileNames = fs.readdirSync(postsDirectory);
+
+  const posts = fileNames.map((fileName) => {
+    const slug = fileName.replace(/\.mdx$/, "");
+    const postFilePath = path.join(postsDirectory, fileName);
+    const postContent = fs.readFileSync(postFilePath, "utf-8");
+
+    const { data } = matter(postContent);
+
+    return {
+      slug,
+      title: data.title,
+      date: data.date,
+      excerpt: data.excerpt,
+      category: data.category,
+      image: data.image,
+      content: "",
+    };
+  });
+
+  return posts.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  );
+}
+
+export async function getPost(slug: string): Promise<Post | null> {
+  const postFilePath = path.join(postsDirectory, `${slug}.mdx`);
+
+  if (!postFilePath) {
+    return null;
+  }
+
+  const postContent = fs.readFileSync(postFilePath, "utf8");
+
+  const { data, content } = matter(postContent);
+
+  const processedContent = (
+    await remark().use(html).process(content)
+  ).toString();
+
+  return {
+    slug,
+    title: data.title,
+    date: data.date,
+    excerpt: data.excerpt,
+    category: data.category,
+    image: data.image,
+    content: processedContent,
+  };
+}
