d8ad795877ed7363ba47b3ff0f947688189f26af
Oscar Castaneda <oscar.castaneda.developer@gmail.com>
2025-05-22 18:50:55 -0600

    fix(seo): correct base url

--- app/blog/metadata.ts
+++ app/blog/metadata.ts
@@ -8,20 +8,24 @@ export async function generateMetadata(): Promise<Metadata> {
     .map((post) => post.title)
     .join(", ");
 
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const url = `${baseUrl}/blog`;
+  const featuredImagePath = "/blog-featured.webp";
+
   return {
     title: "Daily dev log | Development Blog",
     description: `A developer's journal of progress sharing what I learn as I build. Recent posts: ${recentPostTitles}`,
     alternates: {
-      canonical: "https://www.oscarcastaneda.dev/blog",
+      canonical: url,
     },
     openGraph: {
       title: "Daily dev log | Development Blog",
       description: `A developer's journal of progress sharing what I learn as I build. Recent posts: ${recentPostTitles}`,
       type: "website",
-      url: "https://www.oscarcastaneda.dev/blog",
+      url,
       images: [
         {
-          url: "https://www.oscarcastaneda.dev/blog-featured.webp",
+          url: `${baseUrl}${featuredImagePath}`,
           width: 1200,
           height: 630,
           alt: "Daily dev log featured image",
@@ -32,7 +36,7 @@ export async function generateMetadata(): Promise<Metadata> {
       card: "summary_large_image",
       title: "Daily dev log | Development Blog",
       description: `A developer's journal of progress sharing what I learn as I build.`,
-      images: ["https://www.oscarcastaneda.dev/blog-featured.webp"],
+      images: [`${baseUrl}${featuredImagePath}`],
     },
   };
 }
--- app/metadata.ts
+++ app/metadata.ts
@@ -1,22 +1,26 @@
 import { Metadata } from "next";
 
 export function generateMetadata(): Metadata {
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+
+  const featuredImagePath = "/blog-featured.webp";
+
   return {
     title: "Oscar | Financial Software Developer",
     description:
       "I develop financial software with emphasis on visual design. Check out my projects and portfolio.",
     alternates: {
-      canonical: "https://www.oscarcastaneda.dev",
+      canonical: baseUrl,
     },
     openGraph: {
       title: "Oscar | Financial Software Developer",
       description:
         "I develop financial software with emphasis on visual design. Check out my projects and portfolio.",
       type: "website",
-      url: "https://www.oscarcastaneda.dev",
+      url: baseUrl,
       images: [
         {
-          url: "https://www.oscarcastaneda.dev/blog-featured.webp",
+          url: `${featuredImagePath}`,
           width: 1200,
           height: 630,
           alt: "Oscar - Financial Software Developer",
@@ -28,7 +32,7 @@ export function generateMetadata(): Metadata {
       title: "Oscar | Financial Software Developer",
       description:
         "I develop financial software with emphasis on visual design.",
-      images: ["https://www.oscarcastaneda.dev/blog-featured.webp"],
+      images: [`${featuredImagePath}`],
     },
   };
 }
