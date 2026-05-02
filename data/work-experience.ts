0820aed174ffd40987e69df9423e78dc6b8e6f9e
Oscar Castaneda <oscar.castaneda.developer@gmail.com>
2026-05-02 12:26:03 -0600

    feat: add work experience data

---
+++ data/work-experience.ts
@@ -0,0 +1,44 @@
+export const WORK_EXPERIENCE = [
+  {
+    name: "wizeline",
+    role: "lead software engineer",
+    from: "2023",
+    to: null,
+  },
+  {
+    name: "nimble gravity",
+    role: "lead software engineer",
+    from: "2022",
+    to: "2023",
+  },
+  {
+    name: "eetech",
+    role: "senior software engineer",
+    from: "2021",
+    to: "2022",
+  },
+  {
+    name: "cbqa solutions",
+    role: "senior frontend developer",
+    from: "2019",
+    to: "2021",
+  },
+  {
+    name: "blue trail software",
+    role: "frontend developer",
+    from: "2018",
+    to: "2019",
+  },
+  {
+    name: "softtek",
+    role: "frontend developer",
+    from: "2013",
+    to: "2017",
+  },
+  {
+    name: "aquarelab",
+    role: "web developer",
+    from: "2012",
+    to: "2012",
+  },
+];
