e27865557f5e843c1b8e1850f3e6003fec9257a8
Oscar Castaneda <143295025+oscar-castaneda-dev@users.noreply.github.com>
2026-05-15 22:18:52 -0600

    feat: add empty post page at /blog/[slug]

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,13 @@
+interface BlogPostProps {
+  params: Promise<{ slug: string }>;
+}
+
+export default async function BlogPost({ params }: BlogPostProps) {
+  const { slug } = await params;
+
+  return (
+    <div>
+      <p>{slug}</p>
+    </div>
+  );
+}
