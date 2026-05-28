9e72dad67831fcb59fea5ec89566b36f41ec97c1
Oscar Castaneda <143295025+oscar-castaneda-dev@users.noreply.github.com>
2026-05-28 09:37:55 -0600

    feat: add content security policy headers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,31 @@
 import type { NextConfig } from "next";
 
+const ContentSecurityPolicy = `
+  default-src 'self';
+  script-src 'self' 'unsafe-inline' 'unsafe-eval';
+  style-src 'self' 'unsafe-inline';
+  img-src 'self' data: blob:;
+  font-src 'self';
+  connect-src 'self';
+  frame-ancestors 'none';
+`
+  .replace(/\n/g, " ")
+  .trim();
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value: ContentSecurityPolicy,
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
