d87181378d0769d2ee6e4794c22feb33a1157948
Oscar Castaneda <143295025+oscar-castaneda-dev@users.noreply.github.com>
2026-05-28 09:43:51 -0600

    revert: remove content security policy headers

--- next.config.ts
+++ next.config.ts
@@ -1,31 +1,7 @@
 import type { NextConfig } from "next";
 
-const ContentSecurityPolicy = `
-  default-src 'self';
-  script-src 'self' 'unsafe-inline' 'unsafe-eval';
-  style-src 'self' 'unsafe-inline';
-  img-src 'self' data: blob:;
-  font-src 'self';
-  connect-src 'self';
-  frame-ancestors 'none';
-`
-  .replace(/\n/g, " ")
-  .trim();
-
 const nextConfig: NextConfig = {
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "Content-Security-Policy",
-            value: ContentSecurityPolicy,
-          },
-        ],
-      },
-    ];
-  },
+  /* config options here */
 };
 
 export default nextConfig;
